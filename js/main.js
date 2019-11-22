8d7ed9e9b9e2fb0190661c1f55f0ff73017fbd70
PyFellow <50703361+pyfellow@users.noreply.github.com>
2019-11-22 20:50:15 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,158 @@
+// bootstrap sticky header
+$(document).on("scroll", function () {
+    if ($(document).scrollTop() > 0) {
+        $("#banner").addClass("sticky");
+    } else {
+        $("#banner").removeClass("sticky");
+    }
+});
+// dropdown by hover
+const $dropdown = $(".dropdown");
+const $dropdownToggle = $(".dropdown-toggle");
+const $dropdownMenu = $(".dropdown-menu");
+const showClass = "show";
+
+$(window).on("load resize", function () {
+    if (this.matchMedia("(min-width: 768px)").matches) {
+        $dropdown.hover(
+            function () {
+                const $this = $(this);
+                $this.addClass(showClass);
+                $this.find($dropdownToggle).attr("aria-expanded", "true");
+                $this.find($dropdownMenu).addClass(showClass);
+            },
+            function () {
+                const $this = $(this);
+                $this.removeClass(showClass);
+                $this.find($dropdownToggle).attr("aria-expanded", "false");
+                $this.find($dropdownMenu).removeClass(showClass);
+            }
+        );
+    } else {
+        $dropdown.off("mouseenter mouseleave");
+    }
+});
+// logo script
+$(document).on("scroll", function () {
+    if ($(document).scrollTop() > 0) {
+        $("#logo-d").show("logo-dark")
+        $("#logo-w").hide("logo-white");
+
+
+    } else {
+        $("#logo-w").show("logo-white");
+        $("#logo-d").hide("logo-dark");
+
+    }
+});
+// go to top by one click
+
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('#scroll').fadeIn();
+        } else {
+            $('#scroll').fadeOut();
+        }
+    });
+    $('#scroll').click(function () {
+        $("html, body").animate({
+            scrollTop: 0
+        }, 900);
+        return false;
+    });
+});
+
+// gellery scripts
+let modalId = $('#image-gallery');
+
+$(document)
+    .ready(function () {
+
+        loadGallery(true, 'a.thumbnail');
+
+        //This function disables buttons when needed
+        function disableButtons(counter_max, counter_current) {
+            $('#show-previous-image, #show-next-image')
+                .show();
+            if (counter_max === counter_current) {
+                $('#show-next-image')
+                    .hide();
+            } else if (counter_current === 1) {
+                $('#show-previous-image')
+                    .hide();
+            }
+        }
+
+        /**
+         *
+         * @param setIDs        Sets IDs when DOM is loaded. If using a PHP counter, set to false.
+         * @param setClickAttr  Sets the attribute for the click handler.
+         */
+
+        function loadGallery(setIDs, setClickAttr) {
+            let current_image,
+                selector,
+                counter = 0;
+
+            $('#show-next-image, #show-previous-image')
+                .click(function () {
+                    if ($(this)
+                        .attr('id') === 'show-previous-image') {
+                        current_image--;
+                    } else {
+                        current_image++;
+                    }
+
+                    selector = $('[data-image-id="' + current_image + '"]');
+                    updateGallery(selector);
+                });
+
+            function updateGallery(selector) {
+                let $sel = selector;
+                current_image = $sel.data('image-id');
+                $('#image-gallery-title')
+                    .text($sel.data('title'));
+                $('#image-gallery-image')
+                    .attr('src', $sel.data('image'));
+                disableButtons(counter, $sel.data('image-id'));
+            }
+
+            if (setIDs == true) {
+                $('[data-image-id]')
+                    .each(function () {
+                        counter++;
+                        $(this)
+                            .attr('data-image-id', counter);
+                    });
+            }
+            $(setClickAttr)
+                .on('click', function () {
+                    updateGallery($(this));
+                });
+        }
+    });
+
+// build key actions
+$(document)
+    .keydown(function (e) {
+        switch (e.which) {
+            case 37: // left
+                if ((modalId.data('bs.modal') || {})._isShown && $('#show-previous-image').is(":visible")) {
+                    $('#show-previous-image')
+                        .click();
+                }
+                break;
+
+            case 39: // right
+                if ((modalId.data('bs.modal') || {})._isShown && $('#show-next-image').is(":visible")) {
+                    $('#show-next-image')
+                        .click();
+                }
+                break;
+
+            default:
+                return; // exit this handler for other keys
+        }
+        e.preventDefault(); // prevent the default action (scroll / move caret)
+    });
